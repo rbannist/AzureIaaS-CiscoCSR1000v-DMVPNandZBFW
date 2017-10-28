05811b01dce74f3d3c8fb54c2b463e8c04c57fa9
rbannist <rbannist@hotmail.co.uk>
2017-10-28 20:49:08 +0100

    Updated 16_6 VM-deploy scripts

--- Deploy/2_CSR1000v_VMs/Scripts/PS/16_6/csreusprod102.ps1
+++ Deploy/2_CSR1000v_VMs/Scripts/PS/16_6/csreusprod102.ps1
@@ -2,4 +2,4 @@ $subscriptionName = '???'
 Login-AzureRMAccount
 Select-AzureRmSubscription -SubscriptionName $subscriptionName
 
-New-AzureRmResourceGroupDeployment -Name rb-eus-aaa-prod-csrvm102 -ResourceGroupName rb-eus-aaa-prod-csrvm-rg01 -TemplateFile https://raw.githubusercontent.com/rbannist/Azure-CiscoCSR1000v-DMVPNandZBFW/master/Deploy/2_CSR1000v_VMs/JSON/16_6/template-csr102.json -TemplateParameterUri https://raw.githubusercontent.com/rbannist/Azure-CiscoCSR1000v-DMVPNandZBFW/master/Deploy/2_CSR1000v_VMs/JSON/parameters-csreusprod102.json
+New-AzureRmResourceGroupDeployment -Name rb-eus-aaa-prod-csrvm102 -ResourceGroupName rb-eus-aaa-prod-csrvm-rg01 -TemplateFile https://raw.githubusercontent.com/rbannist/AzureIaaS-CiscoCSR1000v-DMVPNandZBFW/master/Deploy/2_CSR1000v_VMs/JSON/16_6/template-csr102.json -TemplateParameterUri https://raw.githubusercontent.com/rbannist/AzureIaaS-CiscoCSR1000v-DMVPNandZBFW/master/Deploy/2_CSR1000v_VMs/JSON/parameters-csreusprod102.json
--- Deploy/2_CSR1000v_VMs/Scripts/PS/16_6/csrneprod101.ps1
+++ Deploy/2_CSR1000v_VMs/Scripts/PS/16_6/csrneprod101.ps1
@@ -2,4 +2,4 @@
 Login-AzureRMAccount
 Select-AzureRmSubscription -SubscriptionName $subscriptionName
 
-New-AzureRmResourceGroupDeployment -Name rb-ne-aaa-prod-csrvm101 -ResourceGroupName rb-ne-aaa-prod-csrvm-rg01 -TemplateFile https://raw.githubusercontent.com/rbannist/Azure-CiscoCSR1000v-DMVPNandZBFW/master/Deploy/2_CSR1000v_VMs/JSON/16_6/template-csr101.json -TemplateParameterUri https://raw.githubusercontent.com/rbannist/Azure-CiscoCSR1000v-DMVPNandZBFW/master/Deploy/2_CSR1000v_VMs/JSON/parameters-csrneprod101.json
+New-AzureRmResourceGroupDeployment -Name rb-ne-aaa-prod-csrvm101 -ResourceGroupName rb-ne-aaa-prod-csrvm-rg01 -TemplateFile https://raw.githubusercontent.com/rbannist/AzureIaaS-CiscoCSR1000v-DMVPNandZBFW/master/Deploy/2_CSR1000v_VMs/JSON/16_6/template-csr101.json -TemplateParameterUri https://raw.githubusercontent.com/rbannist/AzureIaaS-CiscoCSR1000v-DMVPNandZBFW/master/Deploy/2_CSR1000v_VMs/JSON/parameters-csrneprod101.json
